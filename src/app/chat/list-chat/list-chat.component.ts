66fc86ebd00347d7368c9ca5ec4d7039e7f58fdc
Jose Chavarria <jose.chavarria@bg.com.bo>
2024-01-26 23:57:40 -0400

    formulario mensaje

--- src/app/chat/list-chat/list-chat.component.ts
+++ src/app/chat/list-chat/list-chat.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription, from, interval, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-list-chat',
@@ -14,36 +15,63 @@ export class ListChatComponent implements OnInit {
   uuid: string = '';
   messagesJson: any;
   loading: boolean = true;
-
+  userStorage: any = localStorage.getItem('user')
+  user: any = JSON.parse(this.userStorage);
+  private intervalSubscription: Subscription = new Subscription();
 
   constructor(private formBuilder: FormBuilder, private httpClient: HttpClient, private route: ActivatedRoute) {
-    this.uuid = this.route.snapshot.paramMap.get('uuid') || '';
+    this.uuid = this.route.snapshot.paramMap.get('uuid') ?? '';
     if (this.uuid) {
       this.getMessagesDigitalOceans();
+
+      this.intervalSubscription = interval(3000)
+      .pipe(
+        switchMap(() => from(this.getMessagesDigitalOceansRefresh()))
+      )
+      .subscribe(async (data) => {
+        if (data) {
+          this.messagesJson = data;
+        }
+      });
     }
     this.messageForm = this.formBuilder.group({
       message: ['', Validators.required],
-      userId: ['b04b168e-922c-40f1-8f31-1833f493d1f9', Validators.required],
+      userId: [ this.user.id, Validators.required],
       chatId: [this.uuid, Validators.required],
     });
-  }
-  getMessagesDigitalOceans() {
-    this.httpClient.get(`http://147.182.253.73:4000/message/${this.uuid}`)
-      .subscribe((data: any) => {
-        this.messagesJson = data;
-        console.log(this.messagesJson)
-      });
   }
 
   ngOnInit() {
     this.getChatsDigitalOceans();
   }
 
-  getChatsDigitalOceans() {
-    this.httpClient.get('http://147.182.253.73:4000/chats').subscribe((data: any) => {
+  getMessagesDigitalOceans() {
+    this.httpClient.get(`http://147.182.253.73:4000/message/${this.uuid}`)
+      .subscribe((data: any) => {
+        this.messagesJson = data;
+      });
+  }
+
+  ngOnDestroy(): void {
+    // Detiene el intervalo cuando el componente se destruye
+    if (this.intervalSubscription) {
+      this.intervalSubscription.unsubscribe();
+    }
+  }
+
+  getChatsDigitalOceans():any {
+    const {id} = JSON.parse(this.userStorage)
+    // http://147.182.253.73:4000/chat/host/${id}
+    this.httpClient.get(`http://147.182.253.73:4000/chats`).subscribe((data: any) => {
       this.dataJson = data;
+      console.log(data)
       this.loading = false;
     });
+  }
+
+  getMessagesDigitalOceansRefresh(): any {
+    // Return an observable using 'of'
+    return this.httpClient.get(`http://147.182.253.73:4000/message/${this.uuid}`);
   }
 
   sendMessage() {
